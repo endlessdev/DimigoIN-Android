7e3010db7b18ac3b75392f9e26b902904d66d012
endlessdev <ysw0094@gmail.com>
2016-03-10 20:42:34 +0900

    [ContentActivity] Cleaning code. (Refactoring)

--- app/src/main/java/us/narin/dimigoin/activities/ContentActivity.java
+++ app/src/main/java/us/narin/dimigoin/activities/ContentActivity.java
@@ -3,12 +3,12 @@ package us.narin.dimigoin.activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
@@ -20,7 +20,6 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.BindString;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import com.bumptech.glide.Glide;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -134,37 +133,28 @@ public class ContentActivity extends AppCompatActivity {
                 //JSON 응답 (타입오류)의 이상으로 제일 첫번째의 데이터를 가져옵니다.
                 final ContentDetail.Data tmpModel = response.body().getResultData().get(0);
                 final List<File> fileList = response.body().getResultData().get(0).getFiles();
+                final String curruntTime = new TimeStamp(getApplicationContext()).getTimes(tmpModel.getArticle().getPostTime());
+                final char authorLastName = tmpModel.getArticle().getAuthorName().charAt(0);
+                final BoardCommentAdapter commentAdapter = new BoardCommentAdapter(getApplicationContext(), response.body().getResultData().get(0).getComments());
+
                 contentView.loadData(Schema.WEBVIEW_DEFAULT_STYLE + tmpModel.getArticle().getContentBody(), Schema.WEBVIEW_DEFAULT_TYPE, null);
 
                 contentAuthor.setText(tmpModel.getArticle().getAuthorName());
-                contentTime.setText(new TimeStamp(getApplicationContext()).getTimes(tmpModel.getArticle().getPostTime()));
-                contentProfile.setText(String.valueOf(tmpModel.getArticle().getAuthorName().charAt(0)));
+
+                contentTime.setText(curruntTime);
+                contentProfile.setText(String.valueOf(authorLastName));
 
                 likeCount.setText(bbsLike + tmpModel.getArticle().getGoodCount() + bbsUnit);
                 commentCount.setText(bbsComment + tmpModel.getComments().size() + bbsUnit);
                 viewCount.setText(bbsView + tmpModel.getArticle().getViewCount() + bbsUnit);
+
+
                 if (!tmpModel.getFiles().isEmpty()) {
-                    if (fileList.size() == 1) {
-                        firstFileWrapper.setVisibility(View.VISIBLE);
-                        firstFile.setText(fileList.get(0).getFileName() + "[" + fileList.get(0).getDownloadCount() + "]");
-                        firstFileWrapper.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Schema.FILE_DOWNLOAD + fileList.get(0).getFilePath() + "/" + Session.getUserToken(getApplicationContext())))));
-                    } else if (fileList.size() == 2) {
-                        secondFileWrapper.setVisibility(View.VISIBLE);
-                        secondFile.setText(fileList.get(1).getFileName() + "[" + fileList.get(1).getDownloadCount() + "]");
-                        secondFileWrapper.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Schema.FILE_DOWNLOAD + fileList.get(1).getFilePath() + "/" + Session.getUserToken(getApplicationContext())))));
-                    }
+                    setFileVisibillity(fileList);
                 }
 
                 if (!tmpModel.getComments().isEmpty()) {
-                    commentWrapper.setVisibility(View.VISIBLE);
-                    //fix NestiedScroollView in RecyclerView
-                    commentView.setLayoutManager(new NestedInRecyclerManager(getApplicationContext()));
-                    commentView.setHasFixedSize(false);
-                    commentView.setNestedScrollingEnabled(false);
-
-                    BoardCommentAdapter commentAdapter = new BoardCommentAdapter(getApplicationContext(), response.body().getResultData().get(0).getComments());
-                    commentView.setAdapter(commentAdapter);
-
+                    setWhenHasComments(commentAdapter);
                 }
             }
 
@@ -175,36 +165,58 @@ public class ContentActivity extends AppCompatActivity {
         });
 
 
+        commentSubmit.setOnClickListener(v -> submitComment());
+
     }
 
-    @OnClick(R.id.bbs_content_detail_comment_submit)
-    void onSubmitComment() {
-        final String commentContent = commentField.getText().toString();
+    private void submitComment() {
+        final Handler commentWritingHandler = new Handler();
+        final Thread commentWritingThread = new Thread(() -> {
+            commentWritingHandler.post(() -> requestComment(contentId, commentField.getText().toString(), boardId));
+        });
+        commentWritingThread.start();
+    }
 
-        new Thread(() -> {
-            try {
-                Connection.Response response = Jsoup.connect(Schema.ENDPOINT + "/bbs/write_comment_update.php")
-                        .cookie(Schema.LOGIN_COOKIE_KEY, Session.getUserCookie(getApplicationContext()))
-                        .data("wr_id", contentId.toString())
-                        .data("wr_content", commentContent)
-                        .data("w", "c")
-                        .data("bo_table", boardId).method(Connection.Method.POST)
-                        .execute();
-
-                if (response.statusCode() == 200) {
-                    //댓글 작성 성공
-                } else {
-                    //댓글 작성 실패
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
+    private void requestComment(Integer contentId, String contentComment, String boardTable) {
+        try {
+            Connection.Response response = Jsoup.connect(Schema.ENDPOINT + "/bbs/write_comment_update.php")
+                    .cookie(Schema.LOGIN_COOKIE_KEY, Session.getUserCookie(getApplicationContext()))
+                    .data("wr_id", contentId.toString())
+                    .data("wr_content", contentComment)
+                    .data("w", "c")
+                    .data("bo_table", boardTable).method(Connection.Method.POST)
+                    .execute();
+            if (response.statusCode() == 200) {
+                Log.d(TAG, response.body());
+            } else {
+                Log.d(TAG, "failed");
             }
-        }).start();
+
+            Log.d(TAG, response.body());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+    private void setFileVisibillity(List<File> fileList) {
+        if (fileList.size() == 1) {
+            firstFileWrapper.setVisibility(View.VISIBLE);
+            firstFile.setText(fileList.get(0).getFileName() + "[" + fileList.get(0).getDownloadCount() + "]");
+            firstFileWrapper.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Schema.FILE_DOWNLOAD + fileList.get(0).getFilePath() + "/" + Session.getUserToken(getApplicationContext())))));
+        } else if (fileList.size() == 2) {
+            secondFileWrapper.setVisibility(View.VISIBLE);
+            secondFile.setText(fileList.get(1).getFileName() + "[" + fileList.get(1).getDownloadCount() + "]");
+            secondFileWrapper.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Schema.FILE_DOWNLOAD + fileList.get(1).getFilePath() + "/" + Session.getUserToken(getApplicationContext())))));
+        }
+    }
+
+    private void setWhenHasComments(BoardCommentAdapter commentAdapter) {
+        commentWrapper.setVisibility(View.VISIBLE);
+        //fix NestiedScroollView in RecyclerView
+        commentView.setLayoutManager(new NestedInRecyclerManager(getApplicationContext()));
+        commentView.setHasFixedSize(false);
+        commentView.setNestedScrollingEnabled(false);
+        commentView.setAdapter(commentAdapter);
+
     }
 }
